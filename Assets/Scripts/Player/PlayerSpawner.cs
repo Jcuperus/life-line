f9817459777213b8a0cc92335de0db11e3561dad
BassWasTaken <basvegt+github@gmail.com>
2021-07-04 16:54:52 +0200

    minor
    just removed a note I made earlier

--- Assets/Scripts/Player/PlayerSpawner.cs
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 /// <summary>
 /// MonoBehaviour for object which instantiates a player prefab in the scene.
 /// </summary>
-// Note: it might not be very efficient to use a wholly separate class for this, really all the gameManager would need is the location at which to spawn and the prefab to instantiate, both of which we should be able to give more easily.
 public class PlayerSpawner : MonoBehaviour
 {
     /**************** VARIABLES *******************/
